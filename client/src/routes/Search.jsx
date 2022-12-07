9ae84fbbf1fcb9c8a83f193287b4e54ca7b5493d
LesPantsFancy <mjnrock@umich.edu>
2022-12-07 10:02:13 -0500

    init

--- client/src/routes/Search.jsx
+++ client/src/routes/Search.jsx
@@ -27,59 +27,51 @@ export function Search() {
 		<Grid>
 			{
 				msg.map((item, index) => (
-					<Grid.Row key={ index }>
-						<Grid.Column>
-							<Container>
-								<Header as="h3">
-									{ item.Name }
-								</Header>
+					<>
+						<Grid.Row key={ index }>
+							<Grid.Column>
+								<Container>
+									<Header as="h3">
+										{ item.Name }
+									</Header>
 
-								<Segment>
-									{ item.AddressNumber } { item.Street }, { item.City }, { item.State }
-								</Segment>
+									<Segment>
+										{ item.AddressNumber } { item.Street }, { item.City }, { item.State }
+									</Segment>
 
-								<Table>
-									<Table.Header>
-										<Table.Row>
-											<Table.HeaderCell>Flag</Table.HeaderCell>
-											<Table.HeaderCell>Value</Table.HeaderCell>
-										</Table.Row>
-									</Table.Header>
+									<Table>
+										<Table.Header>
+											<Table.Row>
+												<Table.HeaderCell>Flag</Table.HeaderCell>
+												<Table.HeaderCell>Value</Table.HeaderCell>
+											</Table.Row>
+										</Table.Header>
 
-									<Table.Body>
-										<Table.Row>
-											<Table.Cell>On Premise</Table.Cell>
-											<Table.Cell>{ item.IsOnPremise }</Table.Cell>
-										</Table.Row>
-										<Table.Row>
-											<Table.Cell>Off Premise</Table.Cell>
-											<Table.Cell>{ item.IsOffPremise }</Table.Cell>
-										</Table.Row>
-										<Table.Row>
-											<Table.Cell>Is Ownership Transferable</Table.Cell>
-											<Table.Cell>{ item.IsStatuteOwnershipTransferable }</Table.Cell>
-										</Table.Row>
-										<Table.Row>
-											<Table.Cell>Is Location Transferable</Table.Cell>
-											<Table.Cell>{ item.IsStatuteLocationTransferable }</Table.Cell>
-										</Table.Row>
-									</Table.Body>
-								</Table>
-							</Container>
-						</Grid.Column>
-					</Grid.Row>
+										<Table.Body>
+											<Table.Row>
+												<Table.Cell>On Premise</Table.Cell>
+												<Table.Cell>{ item.IsOnPremise }</Table.Cell>
+											</Table.Row>
+											<Table.Row>
+												<Table.Cell>Off Premise</Table.Cell>
+												<Table.Cell>{ item.IsOffPremise }</Table.Cell>
+											</Table.Row>
+											<Table.Row>
+												<Table.Cell>Is Ownership Transferable</Table.Cell>
+												<Table.Cell>{ item.IsStatuteOwnershipTransferable }</Table.Cell>
+											</Table.Row>
+											<Table.Row>
+												<Table.Cell>Is Location Transferable</Table.Cell>
+												<Table.Cell>{ item.IsStatuteLocationTransferable }</Table.Cell>
+											</Table.Row>
+										</Table.Body>
+									</Table>
+								</Container>
+							</Grid.Column>
+						</Grid.Row>
 
-					// <li key={ index } style={ { display: "flex" } }>
-					// 	<div>{ item.LegalName }</div>
-					// 	&nbsp;:&nbsp;
-					// 	<div>{ item.DBAName }</div>
-					// 	&nbsp;:&nbsp;
-					// 	<div>{ item.Name }</div>
-					// 	&nbsp;:&nbsp;
-					// 	<div>{ item.AddressNumber } { item.Street }</div>
-					// 	&nbsp;:&nbsp;
-					// 	<div>{ item.IsStatuteLocationTransferable }</div>
-					// </li>
+						<hr />
+					</>
 				))
 			}
 		</Grid>
